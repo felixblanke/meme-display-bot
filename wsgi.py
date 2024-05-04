ed7ed2df8f24516b442f1276938bd7a20f682214
Felix Blanke <info@fblanke.de>
2024-05-05 00:56:20 +0200

    Refactor

--- wsgi.py
+++ wsgi.py
@@ -13,41 +13,39 @@ app.config.from_file("config.json", load=json.load)
 
 @app.route("/webhook", methods=["POST"])
 def webhook_receiver():
+    return_value = "ok", 200
+
     data = request.json
 
-    try:
-        if "photo" not in data["message"]:
-            return "ok", 200
-        file_request = {"file_id": data["message"]["photo"][-1]["file_id"]}
+    if "photo" not in data["message"]:
+        return return_value
+    file_request = {"file_id": data["message"]["photo"][-1]["file_id"]}
 
-        response = requests.post(
-            f"https://api.telegram.org/bot{app.config['TOKEN']}/getFile", file_request
-        )
-        if response.status_code != 200:
-            return "ok", 200
+    response = requests.post(
+        f"https://api.telegram.org/bot{app.config['TOKEN']}/getFile", file_request
+    )
+    if response.status_code != 200:
+        return return_value
 
-        response = response.json()
+    response = response.json()
 
-        if not response["ok"]:
-            return "ok", 200
+    if not response["ok"]:
+        return return_value
 
-        file_path = response["result"]["file_path"]
+    file_path = response["result"]["file_path"]
 
-        image_response = requests.get(
-            f"https://api.telegram.org/file/bot{app.config['TOKEN']}/{file_path}"
-        )
+    image_response = requests.get(
+        f"https://api.telegram.org/file/bot{app.config['TOKEN']}/{file_path}"
+    )
 
-        suffix = Path(file_path).suffix
-        name = str(uuid.uuid4()) + suffix
+    suffix = Path(file_path).suffix
+    name = str(uuid.uuid4()) + suffix
 
-        with (Path(app.static_folder) / name).open("wb") as handle:
-            for data in image_response.iter_content():
-                handle.write(data)
+    with (Path(app.static_folder) / name).open("wb") as handle:
+        for data in image_response.iter_content():
+            handle.write(data)
 
-    except Exception as ex:
-        raise ex
-
-    return "ok", 200
+    return return_value
 
 
 @app.route(f"/{static_folder_name}/", methods=["GET"])
